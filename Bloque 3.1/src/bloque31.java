325d6572131f4a09d1a3d81304bab910ab6774fc
ALEPA <ALEPA@ALEPA-PC>
2016-08-23 13:40:13 -0300

    bloque 3.1

---
+++ Bloque 3.1/src/bloque31.java
@@ -0,0 +1,21 @@
+
+public class bloque31 {
+	public static void main(String[] arg){
+		int i = 0;
+		int j;
+	
+		while(i < 5){
+			System.out.println(i+" ");
+			i += 1;
+			
+		}
+		System.out.println("Estoy fuera del bucle");
+		
+		for(j = 0; j < 5; j += 1){
+			System.out.println(j+" ");
+			
+		}
+		System.out.println("Estoy fuera del bucle for");
+	}
+
+}
