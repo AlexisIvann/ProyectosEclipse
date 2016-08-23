1a0ba426f3f1adca6a85c7d082200f2817427224
ALEPA <ALEPA@ALEPA-PC>
2016-08-23 18:26:35 -0300

    Bloque 1.3

---
+++ Bloque 1.3/src/Bloque13.java
@@ -0,0 +1,18 @@
+
+public class Bloque13 {
+public static void main(String[] arg){
+	int a,b;
+	a = 25;
+	b = 10;
+	
+	a +=b;// a= a + b;
+	
+	System.out.println("El valor de c es:"+a);
+	
+	a -= b;
+	System.out.println("El valor de a es:"+a);
+	
+	
+	
+}
+}
