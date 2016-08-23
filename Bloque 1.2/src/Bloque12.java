02d367dc6bf9df12773a53880c82a806c935c37b
ALEPA <ALEPA@ALEPA-PC>
2016-08-23 18:25:47 -0300

    Bloque 1.2

---
+++ Bloque 1.2/src/Bloque12.java
@@ -0,0 +1,14 @@
+
+public class Bloque12 {
+public static void main(String[] arg){
+	int x;
+	double y;
+char z;
+	x = 15;
+	
+	y = 7.97;
+	z = 'a';
+	System.out.println("El valor de x es: "+x +"El valor de y es: "+y);
+	System.out.println("El valor de z es:"+z);
+}
+}
