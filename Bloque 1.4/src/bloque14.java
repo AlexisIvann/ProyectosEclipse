a89e169a0bb35a26a1dc3bacc559d99b70784b01
ALEPA <ALEPA@ALEPA-PC>
2016-08-23 18:27:16 -0300

    Bloque 1.4

---
+++ Bloque 1.4/src/bloque14.java
@@ -0,0 +1,19 @@
+//API Conjunto de muchisimas clases= conjunto de ordenes. Scanner = Recoge lo que introducimos por teclado y mete el valor en un atributo.
+
+import java.util.Scanner; 
+
+public class bloque14 {
+public static void main(String [] arg){
+	int a;
+	Scanner S = new Scanner(System.in);
+	
+	
+	System.out.println("Introduce un valor por teclado: ");
+	a = S.nextInt();
+	
+	
+	
+	
+	System.out.println("El valor de a es: "+a);
+}
+}
