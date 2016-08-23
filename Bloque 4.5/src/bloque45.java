e9867b48700ac31c3089ae7767819bc46adc80f5
ALEPA <ALEPA@ALEPA-PC>
2016-08-23 17:31:41 -0300

    bloque 4.5

---
+++ Bloque 4.5/src/bloque45.java
@@ -0,0 +1,19 @@
+//Compracion de strings con cadena de caracterees.
+// concanetación de strings a += "es el mejor canal";
+public class bloque45 {
+
+	public static void main(String[] args) {
+		String a = "Empieza a Programar"; // variables de tipo string vamos a poder almacenar frases
+//System.out.println(a);// mostrar los strings 
+//System.out.println(a.length()); // mostrar cantidad de letras que tiene
+
+String b = "Empieza a Programar";
+if (a == b){
+	System.out.println("Son iguales.");
+}
+else{
+	System.out.println("No son iguales.");
+}
+	}
+
+}
